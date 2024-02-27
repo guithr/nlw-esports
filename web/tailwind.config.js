1f7c45f641fabd9961cff0613e5864ee540700db
Guilherme Matos <guilherme.thrmatos@gmail.com>
2024-02-26 23:19:17 -0300

    Add Phosphor icon

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -4,7 +4,11 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        galaxy: "url(./background-galaxy.png)",
+        "background-galaxy": "url(./background-galaxy.png)",
+        "nlw-gradient":
+          "linear-gradient(89.86deg, #9572FC 11.08%, #43E7AD 46.94%, #E1D55D 80.57%)",
+        "game-gradient":
+          "linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)",
       },
     },
   },
