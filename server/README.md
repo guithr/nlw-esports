99ba7312fe5ecdc5c56940045aee93f22a439be7
Guilherme Matos <guilherme.thrmatos@gmail.com>
2024-03-04 23:18:16 -0300

    Added Prisma ORM to the project

---
+++ server/README.md
@@ -0,0 +1,28 @@
+# Back end
+
+##
+
+### Game
+
+id
+title
+bannerUrl
+
+### AD
+
+ig
+gameId
+name
+yearsPlaying
+discord
+weekDays
+hourStart
+hourEnd
+useVoiceChannel
+
+## Casos de uso
+
+- Listagem de games com contagem de anúncios
+- Criação de novo anúncio
+- Listagem de anúncios por game
+- Buscar discord pelo ID do anúncio
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,19 @@ import express from "express";
 
 const app = express();
 
-app.get("/ads", (request, response) => {
+app.get("/games", (request, response) => {
+  return response.json([])
+})
+
+app.post("/ads", (request, response) => {
+  return response.status(201).json([])
+})
+
+app.get("/games/:id/ads", (request, response) => {
+
+  // const gameId = request.params.id
+
+
   return response.json([
     {
       id: 1,
@@ -26,6 +38,14 @@ app.get("/ads", (request, response) => {
       id: 4,
     }
   ]);
+});
+
+
+app.get("/ads/:id/discord", (request, response) => {
+
+ // const adId = request.params.id
+
+  return response.json([]);
 });
 
 app.listen(3333);
